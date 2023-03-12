5494b54ae52fc067f8c29b86e5f13177b57b8dd1
Aoi Togasaki <aoi.togasaki@icloud.com>
2023-03-12 01:17:26 +0000

    Initial Commit3

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,21 +1,25 @@
 class PostImagesController < ApplicationController
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page])
   end
 
   def new
     @post_image = PostImage.new
   end
 
-   def create
+  def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
-    @post_image.save
-    redirect_to post_images_path
-   end
+    if @post_image.save
+      redirect_to post_images_path
+    else
+      render :new
+    end
+  end
 
   def show
      @post_image = PostImage.find(params[:id])
+     @post_comment = PostComment.new
   end
 
   def destroy
